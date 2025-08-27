e4d781ee6aa19476e356690ad7b28d5af2512e8e
Joseph Lee <joel@joeworks.com>
2025-08-27 13:44:50 +0100

    Fix some fish config issues

--- dot_config/private_fish/conf.d/00_init.fish
+++ dot_config/private_fish/conf.d/00_init.fish
@@ -1,11 +1,8 @@
-
 if test -z $DOTFILES_HOME
     if test -e $HOME/Dotfiles/init.sh
         set -Ux DOTFILES_HOME $HOME/Dotfiles
     else if test -e $HOME/.dotfiles/init.sh
         set -Ux DOTFILES_HOME $HOME/.dotfiles
-    else
-        echo "WARNING: Unable to find DOTFILES_HOME !"
     end
 end
 
