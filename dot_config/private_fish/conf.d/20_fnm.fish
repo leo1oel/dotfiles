4a2c849c3743537003a450012e68f4cd3cd6cec3
leo <liuym23@mails.tsinghua.edu.cn>
2025-10-19 01:27:19 +0800

    Fix fnm PATH initialization in Fish shell

--- dot_config/private_fish/conf.d/20_fnm.fish
+++ dot_config/private_fish/conf.d/20_fnm.fish
@@ -1,3 +1,9 @@
+# Add fnm to PATH if it exists
+if test -d "$HOME/.local/share/fnm"
+    fish_add_path "$HOME/.local/share/fnm"
+end
+
+# Initialize fnm if available
 if type -q fnm
     fnm env | source
 end
