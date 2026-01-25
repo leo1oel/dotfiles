b32fc3e22ed792cd60750b81b6254ad94dd86f2f
leo <liuym23@mails.tsinghua.edu.cn>
2026-01-25 16:15:19 +0800

    Disable startup system banner for neofetch and fastfetch in Fish configuration

--- dot_config/private_fish/conf.d/91_fastfetch.fish
+++ dot_config/private_fish/conf.d/91_fastfetch.fish
@@ -1,7 +1,8 @@
-if status is-interactive
-    if type -q neofetch
-        neofetch
-    else if type -q fastfetch
-        fastfetch 2>/dev/null
-    end
-end
+# Disabled: startup system banner (neofetch/fastfetch)
+# if status is-interactive
+#     if type -q neofetch
+#         neofetch
+#     else if type -q fastfetch
+#         fastfetch 2>/dev/null
+#     end
+# end
