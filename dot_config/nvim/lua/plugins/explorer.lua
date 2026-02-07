1350f2b4942339514d2bbc632fd83fe444ea80ff
leo <liuym23@mails.tsinghua.edu.cn>
2026-02-07 10:06:29 -0800

    feat: update explorer configuration for improved functionality and behavior

--- dot_config/nvim/lua/plugins/explorer.lua
+++ dot_config/nvim/lua/plugins/explorer.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
@@ -11,16 +12,19 @@ return {
       { "<leader>e", "<cmd>Neotree toggle left reveal<cr>", desc = "Explorer" },
     },
     init = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
       vim.cmd([[
         augroup dotfiles_auto_explorer
           autocmd!
-          autocmd VimEnter * if argc() > 0 | Neotree show left reveal | endif
+          autocmd VimEnter * if argc() > 0 && isdirectory(argv(0)) | Neotree show left reveal | endif
         augroup END
       ]])
     end,
     opts = {
       close_if_last_window = false,
       filesystem = {
+        hijack_netrw_behavior = "open_default",
         follow_current_file = { enabled = true },
         filtered_items = {
           hide_dotfiles = false,
