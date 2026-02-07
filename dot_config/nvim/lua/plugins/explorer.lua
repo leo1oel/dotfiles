44cdd30a395a88d2a03ee8a02129797c9dc41a41
leo <liuym23@mails.tsinghua.edu.cn>
2026-02-07 11:14:49 -0800

    feat: simplify explorer plugin configuration and disable neo-tree

--- dot_config/nvim/lua/plugins/explorer.lua
+++ dot_config/nvim/lua/plugins/explorer.lua
@@ -1,35 +1,20 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    keys = {
-      { "<leader>e", "<cmd>Neotree toggle left reveal filesystem<cr>", desc = "Explorer" },
-    },
-    init = function()
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-    end,
+    enabled = false,
+  },
+  {
+    "folke/snacks.nvim",
     opts = {
-      close_if_last_window = false,
-      filesystem = {
-        hijack_netrw_behavior = "open_default",
-        follow_current_file = { enabled = true },
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-        window = {
-          position = "left",
-          width = 32,
-          mappings = {
-            ["/"] = "fuzzy_finder",
-            ["f"] = "filter_on_submit",
-            ["<c-x>"] = "clear_filter",
+      explorer = {
+        enabled = true,
+        replace_netrw = true,
+      },
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
           },
         },
       },
