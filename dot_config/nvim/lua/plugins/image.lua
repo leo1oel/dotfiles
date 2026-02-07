efdaec9176095924a1d225fce4b46caa0fd7cf6f
leo <liuym23@mails.tsinghua.edu.cn>
2026-02-07 11:59:55 -0800

    feat: enable image.nvim plugin and adjust markdown/neorg integration settings

--- dot_config/nvim/lua/plugins/image.lua
+++ dot_config/nvim/lua/plugins/image.lua
@@ -1,24 +1,18 @@
 return {
   "3rd/image.nvim",
-  enabled = false,
-  dependencies = {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-    },
-  },
+  enabled = true,
   opts = {
     backend = "kitty",
     integrations = {
       markdown = {
-        enabled = true,
+        enabled = false,
         clear_in_insert_mode = false,
         download_remote_images = true,
         only_render_image_at_cursor = false,
         filetypes = { "markdown", "vimwiki" },
       },
       neorg = {
-        enabled = true,
+        enabled = false,
         clear_in_insert_mode = false,
         download_remote_images = true,
         only_render_image_at_cursor = false,
